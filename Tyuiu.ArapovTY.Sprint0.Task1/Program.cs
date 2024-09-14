4ae2bbb5fdd281fec5e434d42c1ff5f013674b6b
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-09-14 19:33:39 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ArapovTY.Sprint0.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.ArapovTY.Sprint0.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Тимофей";
+            int age = 17;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
